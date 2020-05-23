21e6ed8d0e710befa25a6cda27a8620978abd19a
Jason.Cheroske <jason.cheroske@gmail.com>
2020-05-23 12:47:27 -0400

    Reformated Code

--- src/main/java/typicals/AlchemicalExpansion/AlchemcialExpansion.java
+++ src/main/java/typicals/AlchemicalExpansion/AlchemcialExpansion.java
@@ -2,10 +2,10 @@ package typicals.AlchemicalExpansion;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid= AlchemcialExpansion.MODID)
+@Mod(modid = AlchemcialExpansion.MODID)
 public class AlchemcialExpansion {
 
     public static final String MODID = "alchemicalexpansion";
-
+        
 }
 
