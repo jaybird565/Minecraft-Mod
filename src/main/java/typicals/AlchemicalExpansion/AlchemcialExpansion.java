8ae4fd567e843fed4652589d435d71719e0e80f3
Zach Wilcher <mail@zachwilcher.com>
2020-05-23 12:29:56 -0400

    Added mod class

---
+++ src/main/java/typicals/AlchemicalExpansion/AlchemcialExpansion.java
@@ -0,0 +1,11 @@
+package typicals.AlchemicalExpansion;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid= AlchemcialExpansion.MODID)
+public class AlchemcialExpansion {
+
+    public static final String MODID = "alchemicalexpansion";
+
+}
+
