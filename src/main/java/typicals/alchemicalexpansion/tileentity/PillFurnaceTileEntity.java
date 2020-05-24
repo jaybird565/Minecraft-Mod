8652f1ffd5302db6877c9de3f99a8eff854e3ae6
Zach Wilcher <mail@zachwilcher.com>
2020-05-24 14:18:20 -0400

    implemented tickable for Pill furnace.

--- src/main/java/typicals/alchemicalexpansion/tileentity/PillFurnaceTileEntity.java
+++ src/main/java/typicals/alchemicalexpansion/tileentity/PillFurnaceTileEntity.java
@@ -1,15 +1,19 @@
 package typicals.alchemicalexpansion.tileentity;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.texture.ITickableTextureObject;
+import net.minecraft.util.ITickable;
 import typicals.alchemicalexpansion.block.ModBlocks;
 
-public class PillFurnaceTileEntity extends ItemHandlerTileEntity {
+public class PillFurnaceTileEntity extends ItemHandlerTileEntity implements ITickable {
 
     public static final int SIZE = 11;
 
     public static final Block[] validBlocks = {ModBlocks.PILL_FURNACE_BLOCK};
 
     public static final String name = "pill_furnace";
+
+    private int counter = 0;
 
     @Override
     public int size() {
@@ -21,4 +25,21 @@ public class PillFurnaceTileEntity extends ItemHandlerTileEntity {
         return validBlocks;
     }
 
+    @Override
+    public void update() {
+        //don't update on client side
+        if (world.isRemote) {
+            return;
+        }
+
+        if (counter == 20) {
+
+            //do something once a second
+
+            counter = 0;
+        } else {
+            counter++;
+        }
+
+    }
 }
