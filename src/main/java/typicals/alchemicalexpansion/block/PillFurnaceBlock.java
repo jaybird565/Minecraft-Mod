4d73e60cfc158dafee0ad2f834e7bfa9e800b3d2
Jason.Cheroske <jason.cheroske@gmail.com>
2020-05-25 22:39:20 -0400

    Added the updatePillFurnaceState method which will allow for the furnace to chang blockstates when activated

--- src/main/java/typicals/alchemicalexpansion/block/PillFurnaceBlock.java
+++ src/main/java/typicals/alchemicalexpansion/block/PillFurnaceBlock.java
@@ -16,6 +16,8 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import typicals.alchemicalexpansion.AlchemicalExpansion;
+import typicals.alchemicalexpansion.handler.BlockRegistrationHandler;
+import typicals.alchemicalexpansion.handler.ModelRegistrationHandler;
 import typicals.alchemicalexpansion.tileentity.PillFurnaceTileEntity;
 
 import javax.annotation.Nonnull;
@@ -37,17 +39,19 @@ public class PillFurnaceBlock extends ModBlockTileEntity {
 
     public PillFurnaceBlock(boolean active) {
         super(pathOff);
-        if(active){
+        if (active) {
             this.path = pathOn;
             this.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
             this.setLightLevel(0.900F);
         }
+        PillFurnaceBlock.active = active;
         this.init();
-        this.active = active;
     }
 
     private void init() {
-        this.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+        if (!PillFurnaceBlock.active) {
+            this.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+        }
         this.setSoundType(SoundType.STONE);
         this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
         this.setHardness(1.5f);
@@ -104,31 +108,33 @@ public class PillFurnaceBlock extends ModBlockTileEntity {
 
     @Nonnull
     @Override
-    public Item getItemDropped(IBlockState state, Random rand, int fortune)
-    {
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
         return Item.getItemFromBlock(ModBlocks.PILL_FURNACE_BLOCK_OFF);
     }
 
     @Override
-    public void breakBlock(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull IBlockState state)
-    {
-        if (!updatingBlock)
-        {
+    public void breakBlock(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
+        if (!updatingBlock) {
             super.breakBlock(world, pos, state);
         }
     }
 
-    public void updatePillFurnaceState(boolean active,World worldin,BlockPos pos){
+    public void updatePillFurnaceState(boolean active, World worldin, BlockPos pos) {
         IBlockState state = worldin.getBlockState(pos);
         TileEntity tile = worldin.getTileEntity(pos);
-        active = this.active;
-        this.updatingBlock = true;
+        active = PillFurnaceBlock.active;
 
-        //TODO
-        if(active = true){
-            //worldin.setBlockState(pos,  );
-        }else{
-            //worldin.setBlockState(pos, state.withProperty();
+        this.updatingBlock = true;
+        if (active = true) {
+            worldin.setBlockState(pos, ModBlocks.PILL_FURNACE_BLOCK_ON.getDefaultState().withProperty(BlockHorizontal.FACING, state.getValue(BlockHorizontal.FACING)));
+        } else {
+            worldin.setBlockState(pos, ModBlocks.PILL_FURNACE_BLOCK_OFF.getDefaultState().withProperty(BlockHorizontal.FACING, state.getValue(BlockHorizontal.FACING)));
+        }
+        updatingBlock = false;
+
+        if (tile != null) {
+            tile.validate();
+            worldin.setTileEntity(pos, tile);
         }
     }
 
