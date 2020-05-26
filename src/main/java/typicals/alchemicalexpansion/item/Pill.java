505382d8b993f653e69849ac75344fa39a29cbbe
Zach Wilcher <mail@zachwilcher.com>
2020-05-25 23:06:04 -0400

    Worked on pill.

--- src/main/java/typicals/alchemicalexpansion/item/Pill.java
+++ src/main/java/typicals/alchemicalexpansion/item/Pill.java
@@ -1,31 +1,13 @@
 package typicals.alchemicalexpansion.item;
 
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.item.EnumAction;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 
-public class Pill extends ModItem {
+import net.minecraft.item.ItemFood;
+
+public class Pill extends ItemFood {
 
     public static final String path = "pill";
 
     public Pill() {
-        super(path);
-    }
-
-    @Override
-    public EnumAction getItemUseAction(ItemStack stack) {
-        return EnumAction.EAT;
-    }
-
-    @Override
-    public int getMaxItemUseDuration(ItemStack stack) {
-        return 0;
-    }
-
-    @Override
-    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityLivingBase entityLiving) {
-        stack.shrink(1);
-        return null;
+        super(0,0,false);
     }
 }
