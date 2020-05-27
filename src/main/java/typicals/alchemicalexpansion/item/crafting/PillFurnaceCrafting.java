fd0f5d842d39a4575c86c0fe1c4243cf5b357c44
Jason.Cheroske <jason.cheroske@gmail.com>
2020-05-27 13:37:44 -0400

    Added the pill furnace crafting class and the _factories.json to define custome types in recipes

---
+++ src/main/java/typicals/alchemicalexpansion/item/crafting/PillFurnaceCrafting.java
@@ -0,0 +1,14 @@
+package typicals.alchemicalexpansion.item.crafting;
+
+import com.google.gson.JsonObject;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.common.crafting.IRecipeFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+
+public class PillFurnaceCrafting implements IRecipeFactory {
+
+    @Override
+    public IRecipe parse(JsonContext context, JsonObject json) {
+        return null;
+    }
+}
