6f8991485def4a93b2a9a868941d82ff90d614f8
Zach Wilcher <mail@zachwilcher.com>
2020-05-31 17:53:57 -0400

    Refactored PillFurnaceTile update() logic as well as implemented some methods in MachineRecipe and ItemUtil to help determine if the items in the crafting slots of the pill furnace can craft anything described in Recipes.pillFurnaceRecipes

--- src/main/java/typicals/alchemicalexpansion/util/ItemUtil.java
+++ src/main/java/typicals/alchemicalexpansion/util/ItemUtil.java
@@ -4,6 +4,11 @@ package typicals.alchemicalexpansion.util;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ItemUtil {
 
 
@@ -26,5 +31,85 @@ public class ItemUtil {
     public static int getBurnTime(Item item) {
         return 200;
     }
+
+    public static int getTotalBurnTime(ItemStack stack) {
+        return getBurnTime(stack) * stack.getCount();
+    }
+
+
+    /** reduces the count of stack by stack0 if and only if they contain the same item.
+     *
+     *  If stack0 contains a greater than or equal amount of items in stack,
+     *  then stack's count is set to 0.
+     */
+    public static void subtractStacks(ItemStack stack, ItemStack stack0) {
+        if(!stack.isItemEqual(stack0)) {
+            return;
+        } else if(stack0.getCount() >= stack.getCount()) {
+            stack.setCount(0);
+        } else {
+            stack.shrink(stack0.getCount());
+        }
+
+    }
+
+    /** tells whether a list of ItemStacks contains another ItemStack
+     *
+     * @param items a list that may contain itemIn
+     * @param itemIn a item that may be in items
+     * @return whether itemIn's Item is in items and if itemIn's count is greater than or equal to the matching ItemStack in items
+     */
+    public static boolean containsItemStack(List<ItemStack> items, ItemStack itemIn) {
+
+        for(ItemStack  item: items) {
+            if(item.isItemEqual(itemIn) ) {
+                if(item.getCount() >= itemIn.getCount()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
+
+    }
+
+
+    public static boolean containsItem(List<ItemStack> items, ItemStack itemIn) {
+        for(ItemStack item : items) {
+            if(item.getItem() == itemIn.getItem()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** takes ItemStacks and joins stacks with matching items compressing it
+     *
+     * @param items the list that will "compressed"
+     * @return the compressed list of ItemStacks
+     */
+    public static List<ItemStack> compress(List<ItemStack> items) {
+        Map<Item, Integer> map = new HashMap<Item, Integer>(items.size());
+
+        for(ItemStack item: items) {
+            if(map.containsKey(item.getItem())) {
+                map.put(item.getItem(), item.getCount() + map.get(item));
+            } else {
+                map.put(item.getItem(), item.getCount());
+            }
+        }
+        List<ItemStack> itemsOut = new ArrayList<ItemStack>();
+
+        for(Map.Entry<Item, Integer> entry : map.entrySet()) {
+            ItemStack stack = new ItemStack(entry.getKey());
+            stack.setCount(entry.getValue());
+            itemsOut.add(stack);
+        }
+
+        return itemsOut;
+
+    }
+
 }
 
