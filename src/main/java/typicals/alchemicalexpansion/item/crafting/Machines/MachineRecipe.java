865350d1fc126b9a18d420a391e74a300c85a6db
Zach Wilcher <mail@zachwilcher.com>
2020-05-31 15:55:26 -0400

    Moved getters and setters for PillFurnaceRecipe into MachineRecipe

--- src/main/java/typicals/alchemicalexpansion/item/crafting/Machines/MachineRecipe.java
+++ src/main/java/typicals/alchemicalexpansion/item/crafting/Machines/MachineRecipe.java
@@ -3,11 +3,42 @@ package typicals.alchemicalexpansion.item.crafting.Machines;
 import net.minecraft.item.ItemStack;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MachineRecipe {
 
-    List<ItemStack> inputs = new ArrayList<>();
-    List<ItemStack> outputs = new ArrayList<>();
+    protected List<ItemStack> inputs;
+
+    protected List<ItemStack> outputs;
+
+    public MachineRecipe() {
+        this.inputs = new ArrayList<ItemStack>();
+        this.outputs = new ArrayList<ItemStack>();
+    }
+
+    public void setInputs(List<ItemStack> inputs) {
+        this.inputs = inputs;
+    }
+
+    public void setOutputs(List<ItemStack> outputs) {
+        this.outputs = outputs;
+    }
+
+    public void setOutputs(ItemStack[] outputs){
+        this.outputs = Arrays.asList(outputs);
+    }
+
+    public void setInputs(ItemStack[] inputs) {
+        this.inputs = Arrays.asList(inputs);
+    }
+
+    public List<ItemStack> getInputs() {
+        return inputs;
+    }
+
+    public List<ItemStack> getOutputs() {
+        return outputs;
+    }
 
 }
--- src/main/java/typicals/alchemicalexpansion/item/crafting/Machines/PillFurnaceRecipe.java
+++ src/main/java/typicals/alchemicalexpansion/item/crafting/Machines/PillFurnaceRecipe.java
@@ -3,31 +3,15 @@ package typicals.alchemicalexpansion.item.crafting.Machines;
 
 import net.minecraft.item.ItemStack;
 
+
 public class PillFurnaceRecipe extends MachineRecipe {
 
-    //sets all the inputs for a recipe
-    public boolean setInputs(ItemStack[] inputs){
-        //TODO turn this into a try catch
-        for(int i = 0; i < inputs.length - 1; i++ ){
-            if(inputs[i] != null){
-                this.inputs.add(inputs[i]);
-            } else {
-                return true;
-            }
+    public ItemStack getOutput() {
+        if(this.outputs.size() < 1) {
+            return ItemStack.EMPTY;
+        } else {
+            return this.outputs.get(0);
         }
-        return true;
-    }
-    //sets all the outputs in a recipe though there technically can only be one.... for now.
-    public boolean setOutputs(ItemStack[] outputs){
-        //TODO turn this into a try catch
-        for(int i = 0; i < outputs.length - 1; i++ ){
-            if(outputs[i] != null){
-                this.outputs.add(outputs[i]);
-            } else {
-                return true;
-            }
-        }
-        return true;
     }
 
 }
